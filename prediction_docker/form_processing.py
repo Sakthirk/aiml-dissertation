df499d0da089b1eec74f03affe60faaa9c9489d3
Sakthirk <sakthi2096@gmail.com>
2025-03-10 23:35:03 +0530

    Update form_processing.py

--- prediction_docker/form_processing.py
+++ prediction_docker/form_processing.py
@@ -200,7 +200,7 @@ class FormProcessing:
             
         def match_features_to_model_columns(self,features,email):
 
-            proficiency_test_score = float(features.get('proficiency_test_score').strip()) if features.get('proficiency_test_score') is not None and features.get('proficiency_test_score') !='' else None
+            proficiency_test_score = float(features.get('proficiency_test_score').strip()) if (features.get('proficiency_test_score') is not None and features.get('proficiency_test_score') !='') else None
 
             feature_dict = {
                 "student_country": [features.get('country_of_residence')],
@@ -212,7 +212,7 @@ class FormProcessing:
                 "intake": [self.get_intake_with_year(features.get('start_date'))],
                 "budget": [self.process_budget(features.get('budget'))],
                 "english_test": [features.get('proficiency_test')],
-                "english_test_score": [features.get('proficiency_test_score')],
+                "english_test_score": [proficiency_test_score],
                 "time_to_study": [self.months_between_dates(features.get('start_date'))],
                 "intro_source": [features.get('how_you_know')],
                 "app_used": [ "Yes" if len(self.get_clickstream_data(email))>= 1 else "No"],
